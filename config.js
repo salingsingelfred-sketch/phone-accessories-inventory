8332ab80d5e986d5cd372f6ba00bf4ed0624120b
salingsingelfred-sketch <salingsingelfred@gmail.com>
2026-09-05 11:12:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // After deploying your Apps Script, paste the URL here.
 
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyOc9s9A6zEQ7uNk_Z_TsVyjibBZWZzcrxtOjofQdj5OS89ZPjG2bK7wih28Dszj_6A/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwdDBfCa06leIQATnvTFTeHC74WdNO-rCMQYAbB2oO18dmk-sg3Td_VPeSw7RvEEQL8/exec",
   APP_TITLE: "Phone Accessories Inventory System",
   VERSION: "1.0.0",
   SESSION_KEY: "pais_session",
